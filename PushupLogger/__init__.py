576df7641ee619f33a7b561a137050a0cfe332f4
gaurav <gauravnautiyal77@gmail.com>
2024-04-30 18:21:05 +0530

    sqlalchemy and mysql connector

--- PushupLogger/__init__.py
+++ PushupLogger/__init__.py
@@ -1,9 +1,24 @@
 ### init.py will be only used once for like setting up database and login features for each instance
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
+
+    username = 'root'
+    password = 'Gmail123'
+    host = 'localhost'
+    port = 3306
+    database_name = 'pushupdatabase'
+
+    db_uri = 'mysql+mysqlconnector://' + username + ':' + password + '@' + host + ':' + str(port) + '/' + database_name
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    ##INITIALIZING our database
+    db.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
--- PushupLogger/auth.py
+++ PushupLogger/auth.py
@@ -1,6 +1,14 @@
 ## LOGIN features
 from flask import Blueprint, render_template, url_for, request, redirect
 
+##For storing password as hash
+from werkzeug.security import generate_password_hash
+
+from .models import user
+
+## by giving filename _init__ it tells python that the module is a package and all the functions are available to use within the directory
+from . import db
+
 ## __name__ represents the name of the current file, it ensures Blueprint can find its resources relative to the file
 ## location
 auth = Blueprint('auth',__name__)
@@ -19,8 +27,23 @@ def signup_post():
     email=request.form.get('exampleInputEmail1')
     password=request.form.get('exampleInputPassword1')
     print(name,email,password)
-    return redirect (url_for('auth.login'))
 
+    UserAlreadyPresent=user.query.filter_by(email=email).first()
+    if UserAlreadyPresent:
+        print("user already present!!!")
+        return redirect(url_for('auth.signup'))
+    
+    else:
+        new_user=user(name=name, email=email, password=generate_password_hash(password))
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            print('User created successfully!')
+            return redirect(url_for('auth.login'))
+        except Exception as e:
+            print(f'Error creating user: {e}')
+            return redirect(url_for('auth.signup'))
+            # Handle the error appropriately (e.g., rollback the session)
 @auth.route('/login',methods=['POST'])
 def login_post():
     name=request.form.get('inputName')
