a747dc25947da2fbbf2c738ceb6c8be89592927b
gaurav <gauravnautiyal77@gmail.com>
2024-04-30 15:50:53 +0530

    signup and login

--- PushupLogger/auth.py
+++ PushupLogger/auth.py
@@ -1,5 +1,5 @@
 ## LOGIN features
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
 
 ## __name__ represents the name of the current file, it ensures Blueprint can find its resources relative to the file
 ## location
@@ -11,3 +11,20 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@auth.route('/signup',methods=['POST'])
+def signup_post():
+    name=request.form.get('inputName')
+    email=request.form.get('exampleInputEmail1')
+    password=request.form.get('exampleInputPassword1')
+    print(name,email,password)
+    return redirect (url_for('auth.login'))
+
+@auth.route('/login',methods=['POST'])
+def login_post():
+    name=request.form.get('inputName')
+    email=request.form.get('exampleInputEmail1')
+    password=request.form.get('exampleInputPassword1')
+    print(name,email,password)
+    return redirect (url_for('main.profile'))
